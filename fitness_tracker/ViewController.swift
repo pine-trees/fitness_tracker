558129c63742b38aa236a9f46b480bf68a07459e
Evgeny Shmeerov <Eugene@Evgenys-MacBook-Pro.local>
2016-09-09 00:39:19 +0300

    added initial xcode project

---
+++ fitness_tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fitness_tracker
+//
+//  Created by Evgeny Shmeerov on 09/09/16.
+//  Copyright © 2016 Eugene Shmeerov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
