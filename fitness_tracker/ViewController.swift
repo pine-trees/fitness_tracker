5db949c38664386d29e1d3ec96a43d5ca3126448
Evgeny Shmeerov <Eugene@Evgenys-MacBook-Pro.local>
2016-09-11 14:46:56 +0300

    added a stopwatch, but the button is all wrong and it's not realtime

--- fitness_tracker/ViewController.swift
+++ fitness_tracker/ViewController.swift
@@ -9,6 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer: NSTimer!
+    var fractions = 0
+    var secconds = 0
+    var minutes = 0
+    var stopwatchRunning = false
+    
+    func timeCalc () {
+        fractions += 1
+        if fractions >= 100 {
+            secconds += 1
+            fractions = 1
+            if secconds >= 60 {
+                minutes += 1
+                secconds = 0
+            }
+        }
+        stopwatchText.text = "\(minutes):\(secconds):\(fractions)"
+    }
+    
+    @IBOutlet weak var stopwatchText: UILabel!
+   
+    @IBAction func stopWatchFire(sender: AnyObject) {
+        if stopwatchRunning == false {
+            stopwatchRunning = true
+            timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "timeCalc", userInfo: nil, repeats: true)
+            
+        } else {
+            timer.invalidate()
+            stopwatchRunning = false
+        }
+    }
+    
+        
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
