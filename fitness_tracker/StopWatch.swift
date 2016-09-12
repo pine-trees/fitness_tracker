d2148ab455297f6b39e7ac215c05bd93b78ca069
Evgeny Shmeerov <pine-trees@users.noreply.github.com>
2016-09-13 02:10:23 +0300

    havent figured it out yet

--- fitness_tracker/StopWatch.swift
+++ fitness_tracker/StopWatch.swift
@@ -8,3 +8,46 @@
 
 import Foundation
 import QuartzCore
+
+class StopWatch {
+    
+    var displayLink: CADisplayLink!
+    var timePassed: CFTimeInterval!
+//    var fractions = 0
+//    var secconds = 0
+//    var minutes = 0
+    var timestring = ""
+    var check = "all"
+    
+    
+    init () {
+        check = "all good"
+        self.displayLink = CADisplayLink(target: self, selector: "displayLinkUpdate")
+        displayLink.paused = true
+        displayLink.addToRunLoop(NSRunLoop.mainRunLoop(), forMode: NSRunLoopCommonModes)
+        
+        self.timePassed = displayLink.timestamp
+        
+    }
+    
+    func displayLinkUpdate (sender: CADisplayLink) {
+        timePassed = timePassed + displayLink.duration
+    }
+    
+    func start () {
+        displayLink.paused = false
+    }
+
+    
+    func timepassedAsString () {
+//        fractions = (Int(timePassed * 100)) % 100
+//        secconds = Int(timePassed) % 60
+//        minutes = (Int(timePassed) / 60) % 60
+        timestring = "\(NSString(format:"%02d",(Int(timePassed / 60) % 60))):\(NSString(format: "%02d", Int(timePassed) % 60)).\(Int(timePassed * 100) % 100)"
+    }
+    
+//    func formatter (time) {
+//       formattedTime = "\(NSString(format: "%02d", time))"
+//    }
+//    
+}
--- fitness_tracker/ViewController.swift
+++ fitness_tracker/ViewController.swift
@@ -12,48 +12,25 @@ import QuartzCore
 
 class ViewController: UIViewController {
     
-    var displayLink: CADisplayLink!
-    var lastDisplayLinkTimeStamp: CFTimeInterval!
+    var stopWatch = StopWatch()
     
     
     @IBOutlet weak var testFractionLbl: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     @IBAction func startStpWatchBtn(sender: AnyObject) {
-        displayLink.paused = !(displayLink.paused)
+        stopWatch.displayLink.paused = !(stopWatch.displayLink.paused)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        //the following is from the tut
-        //initialize display link and make it call linkUpdate function when there is an update available"
-        displayLink = CADisplayLink(target: self, selector: "displayLinkUpdate")
-        
-        //make sure the displayLink is not updating by default
-        displayLink.paused = true
-        
-        //add the displaylink to the runloop(referenced from apple documentation)
-        displayLink.addToRunLoop(NSRunLoop.mainRunLoop(), forMode: NSRunLoopCommonModes)
-        
-        //initial timestamp (elapsed time in human language). essentially we assign the default value to be zero since display link has not fired yet
-        lastDisplayLinkTimeStamp = displayLink.timestamp
+      
     }
 
-    //displayLink update func - all time calculations must take place here
-    func displayLinkUpdate() {
-        //update current time
-        lastDisplayLinkTimeStamp = lastDisplayLinkTimeStamp + displayLink.duration
-        
-        //format current time to last two meaningfull digits ( the "0.2f" is appearantly from obj-c)
-        let fractions = (Int(lastDisplayLinkTimeStamp * 100)) % 100
-        let secconds = Int(lastDisplayLinkTimeStamp) % 60
-        let minutes = (Int(lastDisplayLinkTimeStamp) / 60) % 60
-        
-        //display formated current time, need to add separate label for fractions or for each to prevent "jumping"
-        timeLabel.text = "\(NSString(format: "%02d", minutes)):\(NSString(format: "%02d", secconds)).\(fractions)"
+    func updateLbl(){
+        timeLabel.text = stopWatch.timestring
     }
-
 
 }
 
